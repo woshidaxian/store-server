b8319da3836f37c149fe4830596e67bbefff7070
woshidaxian <huang15057081701@163.com>
2023-09-10 16:35:31 +0800

    计算文件夹size

--- config/config.js
+++ config/config.js
@@ -5,7 +5,8 @@ const path = require('path')
 process.env.PORT = ''
 
 // 云盘根路径，默认为项目根路径下files文件夹
-process.env.ROOT = 'D://PROJECT//file'
+// process.env.ROOT = 'D://PROJECT//file'
+process.env.ROOT = '/Users/gang/desktop/file'
 
 
 module.exports = {
--- src/routers/file.js
+++ src/routers/file.js
@@ -1,7 +1,10 @@
 const Router = require('koa-router');
-const { loadByUrl } = require('../services/file');
+const { loadByUrl, getFolderSize } = require('../services/file');
 const router = new Router()
 const Ret = require('./../../common/ret')
+const fs = require('fs')
+const config = require('./../../config/config')
+const path = require('path')
 const ret = new Ret();
 
 /**
@@ -41,7 +44,19 @@ router.get('/download', async ctx=>{
  * 文件夹大小
  */
 router.get('/foldSize', async ctx=>{
-
+  const foldUrl = ctx.request.query.url
+  try {
+    if(!foldUrl) throw new Error('路径不正确')
+    const URL = path.join(config.FILE_ROOT, foldUrl);
+    if (!fs.existsSync(URL)) {
+      throw new Error('路径不正确')
+    }else{
+      const size = getFolderSize(URL)
+      ctx.body = ret.success(size)
+    }
+  } catch (error) {
+    ctx.body = ret.error(error.toString())
+  }
 })
 
 module.exports = router
